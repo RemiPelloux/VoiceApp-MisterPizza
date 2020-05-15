c5c29e847c09863deb1b9ff6a31aebf6aa3be592
tkmremi <tkmremi@gmail.com>
2020-05-15 13:30:17 +0200

    index.js L22 : Modification Linguistiques

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
+      : "Il y a un probleme, répétez s'il vous plait.";
   
   var speechResponse = {
     google: {
